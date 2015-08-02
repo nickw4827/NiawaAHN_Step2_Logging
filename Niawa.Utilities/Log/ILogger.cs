63ca900bc98eb29e0c59f6ef62122acd87a0748e
nickw4827 <nwashburn@niawa.com>
2015-08-01 20:53:45 -0700

    Initial commit

---
+++ Niawa.Utilities/Log/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niawa.Utilities.Log
+{
+    public interface ILogger
+    {
+        void Log(NiawaLogType logMessageType, string logMessage);
+        void Log(NiawaLogType logMessageType, string logMessage, Exception logException);
+        void Debug(string logMessage);
+        void Info(string logMessage);
+        void Warn(string logMessage);
+        void Error(string logMessage);
+        void Error(string logMessage, Exception logException);
+        void Severe(string logMessage);
+        void Severe(string logMessage, Exception logException);
+        void AddLogWriter(NiawaLogType logMessageType, ILogWriter logWriter);
+    } 
+}
